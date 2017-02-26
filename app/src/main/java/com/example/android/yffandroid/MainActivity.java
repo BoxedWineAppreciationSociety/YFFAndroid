d912e821d730e7a72cad40ab37587819ad31181d
Chris Jewell <chrisjohnjewell@gmail.com>
2017-02-26 17:39:13 +1100

    Add first attempt Navigation Drawer

--- app/src/main/java/com/example/android/yffandroid/MainActivity.java
+++ app/src/main/java/com/example/android/yffandroid/MainActivity.java
@@ -1,9 +1,21 @@
 package com.example.android.yffandroid;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private ActionBarDrawerToggle toggle=null;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +25,31 @@ public class MainActivity extends AppCompatActivity {
         if (getFragmentManager().findFragmentById(R.id.content_fragment) == null) {
             showArtistList();
         }
+
+        ListView drawer = (ListView) findViewById(R.id.drawer);
+        drawer.setAdapter(
+                new ArrayAdapter<>(
+                        this,
+                        R.layout.drawer_row,
+                        getResources().getStringArray(R.array.drawer_rows)));
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.navigation_drawer, menu);
+        return(super.onCreateOptionsMenu(menu));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        toggle.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected(item);
     }
 
     private void showArtistList() {
