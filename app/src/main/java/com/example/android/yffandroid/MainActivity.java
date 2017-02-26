fe2a12f795268f7bb44ae7bc685d2ddf5709e9a8
Chris Jewell <chrisjohnjewell@gmail.com>
2017-02-26 11:13:51 +1100

    Refactor fragment transaction

--- app/src/main/java/com/example/android/yffandroid/MainActivity.java
+++ app/src/main/java/com/example/android/yffandroid/MainActivity.java
@@ -8,8 +8,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        android.app.FragmentTransaction transaction = getFragmentManager().beginTransaction();
         setContentView(R.layout.activity_main);
+
+        if (getFragmentManager().findFragmentById(R.id.content_fragment) == null) {
+            showArtistList();
+        }
+    }
+
+    private void showArtistList() {
+        android.app.FragmentTransaction transaction = getFragmentManager().beginTransaction();
         ArtistsListFragment fragment = new ArtistsListFragment();
         transaction.replace(R.id.content_fragment, fragment);
         transaction.commit();
