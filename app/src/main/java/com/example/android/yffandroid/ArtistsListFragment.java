329573b8647680583ef5b73848dddd2ee46fe310
Chris Jewell <chrisjohnjewell@gmail.com>
2017-03-02 21:44:42 +1100

    Fix getSupportActionBar in Fragment

--- app/src/main/java/com/example/android/yffandroid/ArtistsListFragment.java
+++ app/src/main/java/com/example/android/yffandroid/ArtistsListFragment.java
@@ -2,13 +2,12 @@ package com.example.android.yffandroid;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -102,7 +101,7 @@ public class ArtistsListFragment extends android.app.Fragment implements ArtistL
     }
 
     private void setActionBarColor() {
-        ActionBar artistsActivityActionBar = getSupportActionBar();
+        ActionBar artistsActivityActionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
         artistsActivityActionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.colorYFFOlive)));
 
         artistsActivityActionBar.setTitle("Artists");
