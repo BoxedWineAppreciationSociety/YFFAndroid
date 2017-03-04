a67bebf93e1613d085517d6478f87d2bf960c744
Chris Jewell <chrisjohnjewell@gmail.com>
2017-03-04 13:21:43 +1100

    Extract Artist About to a Fragment

---
+++ app/src/main/java/com/example/android/yffandroid/ArtistDetailPagerAdapter.java
@@ -0,0 +1,10 @@
+package com.example.android.yffandroid;
+
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by chris on 4/3/17.
+ */
+
+public class ArtistDetailPagerAdapter extends FragmentPagerAdapter {
+}
