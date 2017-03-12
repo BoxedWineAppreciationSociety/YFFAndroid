2301c345e00c89f142ea9de0af9d37921e5d2500
Chris Jewell <chrisjohnjewell@gmail.com>
2017-03-12 13:40:39 +1100

    Programatically generate Social Links

---
+++ app/src/main/java/com/example/android/yffandroid/SocialLink.java
@@ -0,0 +1,116 @@
+package com.example.android.yffandroid;
+
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+
+import java.util.Arrays;
+
+/**
+ * Created by chris on 11/3/17.
+ */
+public class SocialLink implements Comparable<SocialLink> {
+    public static final String FACEBOOK = "facebook";
+    public static final String YOUTUBE = "youtube";
+    public static final String TWITTER = "twitter";
+    public static final String ITUNES = "itunes";
+    public static final String SOUNDCLOUD = "soundcloud";
+    public static final String INSTAGRAM = "instagram";
+
+    private final int mColor;
+    private final String mUrl;
+    private final String mTitle;
+
+    public static final String[] socials = {
+       FACEBOOK, YOUTUBE, TWITTER, ITUNES, SOUNDCLOUD, INSTAGRAM
+    };
+
+    SocialLink() {
+        mTitle = "Link";
+        mColor = R.color.colorYFFOlive;
+        mUrl = "http://google.com";
+    }
+
+    SocialLink(String type, String urlString) {
+        mTitle = titleFor(type);
+        mColor = colorFor(type);
+        mUrl = urlString;
+    }
+
+    private static int colorFor(String type) {
+        int color;
+        switch (type) {
+            case FACEBOOK:
+                color = R.color.colorPrimaryDark;
+                break;
+            case YOUTUBE:
+                color = R.color.colorYoutubeRed;
+                break;
+            case TWITTER:
+                color = R.color.colorTwitterBlue;
+                break;
+            case ITUNES:
+                color = R.color.colorItunesGrey;
+                break;
+            case SOUNDCLOUD:
+                color = R.color.colorSoundcloudOrange;
+                break;
+            case INSTAGRAM:
+                color = R.color.colorInstagramBlue;
+                break;
+            default:
+                color = R.color.colorYFFOlive;
+                break;
+
+        }
+        return color;
+    }
+
+    // TODO: Icon instead of title
+    private static String titleFor(String type) {
+        String title;
+        switch (type) {
+            case FACEBOOK:
+                title = "Facebook";
+                break;
+            case YOUTUBE:
+                title = "Youtube";
+                break;
+            case TWITTER:
+                title = "Twitter";
+                break;
+            case ITUNES:
+                title = "iTunes";
+                break;
+            case SOUNDCLOUD:
+                title = "Soundcloud";
+                break;
+            case INSTAGRAM:
+                title = "Instagram";
+                break;
+            default:
+                title = "Link";
+                break;
+
+        }
+        return title;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public int getColor() {
+        return mColor;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    @Override
+    public int compareTo(SocialLink o) {
+        Integer myPosition = Arrays.asList(socials).indexOf(this);
+        Integer theirPosition = Arrays.asList(socials).indexOf(o);
+        return theirPosition.compareTo(myPosition);
+    }
+}
---
+++ app/src/main/java/com/example/android/yffandroid/SocialLinkCollection.java
@@ -0,0 +1,49 @@
+package com.example.android.yffandroid;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by chris on 11/3/17.
+ */
+
+public class SocialLinkCollection {
+    private final Artist mArtist;
+
+    SocialLinkCollection(Artist artist) {
+        mArtist = artist;
+    }
+
+    List<SocialLink> getSocialLinks() {
+        List<SocialLink> list = new LinkedList<>();
+        if (mArtist != null) {
+            for(Map.Entry<String,String> entry : socialLinks()) {
+                String type = entry.getKey();
+                String url = entry.getValue();
+                if (url != null && !url.equals("")) {
+                    list.add(new SocialLink(type, url));
+                }
+            }
+
+        }
+        Collections.sort(list);
+        return list;
+    }
+
+    private Set<Map.Entry<String, String>> socialLinks() {
+        HashMap<String,String> mapping = new HashMap<>();
+        // TODO: Website link
+        mapping.put(SocialLink.FACEBOOK, mArtist.getFacebookUrl());
+        mapping.put(SocialLink.YOUTUBE, mArtist.getYoutubeUrl());
+        mapping.put(SocialLink.TWITTER, mArtist.getTwitterUrl());
+        mapping.put(SocialLink.ITUNES, mArtist.getItunesUrl());
+        mapping.put(SocialLink.SOUNDCLOUD, mArtist.getSoundcloudUrl());
+        mapping.put(SocialLink.INSTAGRAM, mArtist.getInstagramUrl());
+
+        return mapping.entrySet();
+    }
+}
