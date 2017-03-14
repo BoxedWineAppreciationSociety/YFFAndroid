51fd7a7e8d54d313985e150ca7acb56caf20ada3
Chris Jewell <chrisjohnjewell@gmail.com>
2017-03-15 07:54:38 +1100

    Move to TabLayout instead of TitleTabs

--- app/src/main/java/com/example/android/yffandroid/PagerFragment.java
+++ app/src/main/java/com/example/android/yffandroid/PagerFragment.java
@@ -2,6 +2,7 @@ package com.example.android.yffandroid;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
@@ -17,11 +18,14 @@ public class PagerFragment extends android.support.v4.app.Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.about_pager, container, false);
+        View rootView = inflater.inflate(R.layout.pager_fragment, container, false);
         ViewPager pager = (ViewPager) rootView.findViewById(R.id.summary_pager);
         mArtist = ArtistRepo.getArtist("C1C8B5F3-6622-C48C-5162-B1234803B3A6");
 
         pager.setAdapter(buildAdapter());
+
+        TabLayout tabLayout = (TabLayout) rootView.findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(pager);
 
         return rootView;
     }
